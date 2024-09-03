17286c06fccad4ecb8980d0a6f0db926219ea846
Stefan Vet <stefanvet06@gmail.com>
2024-09-03 13:21:45 +0200

    opdracht 1.1 en 1.2 klaar

---
+++ Opdrachten_C_Sharp/Controllers/Block1StefanController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Opdrachten_C_Sharp.Models.Block1;
+
+namespace Opdrachten_C_Sharp.Controllers
+{
+    public partial class Block1Controller 
+    {
+        Me Stefan = new Me();
+
+        public IActionResult Exercise1Stefan()
+        {
+            /*
+             Schrijf een programma dat de volgende informatie van jou op het scherm laat zien:
+
+                    •  Jouw volledige naam
+                    •  Jouw adres
+                    •  Jouw postcode gevolgd door jouw woonplaats (op één regel)
+                    •  Jouw leeftijd
+
+             */
+
+            Stefan.FirstName = "Stefan";
+            Stefan.MiddleName = "";
+            Stefan.LastName = "Vet";
+            Stefan.Address = "Jan Luikenstraat 6";
+            Stefan.PostalCode = "7552XK";
+            Stefan.City = "Hengelo";
+            Stefan.Age = 18;
+
+            return View(Stefan);
+        }
+        public IActionResult Exercise2Stefan()
+        {
+            /*
+             * Schrijf een programma dat jouw naam weergeeft in een box met sterretjes, zoals hieronder weergegeven:
+
+                ********************
+                ***     Marcel   ***
+                ********************
+
+             */
+
+            Stefan.FirstName = "Stefan";
+            Stefan.MiddleName = "";
+            Stefan.LastName = "Vet";
+            Stefan.Address = "Jan Luikenstraat 6";
+            Stefan.PostalCode = "7552XK";
+            Stefan.City = "Hengelo";
+            Stefan.Age = 18;
+
+            return View(Stefan);
+        }
+    }
+}
